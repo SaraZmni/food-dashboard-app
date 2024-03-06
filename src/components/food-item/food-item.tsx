eeb63cff54cd462acbc1681f8dcd2a9006d67a60
Emma <Emma@fanap.ir>
2024-03-06 17:53:54 +0330

    add food item

---
+++ src/components/food-item/food-item.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+
+const FoodItem: FC = () => {
+  return (
+    <div className="bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative w-1/4">
+      <span className="bg-red-100 border border-secondary rounded-full text-secondary text-sm poppins px-4 py-1 inline-block mb-4 ">
+        {/* {foodType} */}
+        شام
+      </span>
+      <img
+        className="w-64 mx-auto transform transition duration-300 hover:scale-105"
+        src={""}
+        // src={image}
+        alt=""
+      />
+      <div className="flex flex-col items-center my-3 space-y-2">
+        <h1 className="text-gray-900 poppins text-lg">{"قرمه سبزی"}</h1>
+        {/* <h1 className="text-gray-900 poppins text-lg">{title}</h1> */}
+        <p className="text-gray-500 poppins text-sm text-center">
+          توضیحات
+          {/* {description.slice(0, 50)} */}
+        </p>
+        {/* <h2 className="text-gray-900 poppins text-2xl font-bold">${price}</h2> */}
+        <h2 className="text-gray-900 poppins text-2xl font-bold">2000</h2>
+        <button
+          className="bg-secondary text-white text-sm px-8 py-2 focus:outline-none poppins rounded-full mt-24 transform transition duration-300 hover:scale-105"
+          //   onClick={handleRoute}
+        >
+          افزودن به سبد خرید
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FoodItem;
---
+++ src/components/food-item/index.ts
@@ -0,0 +1,1 @@
+export {default as FoodItem} from './food-item'
