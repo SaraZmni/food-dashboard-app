e69d100702264cba4a72933359e6870908924127
Sara Zamani <szamani240@gmail.com>
2024-03-21 23:04:05 +0330

    start dashboard component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        primary: '#ebf9fc',
-        secondary:'#fe5634',
-        main:'#c6c8cc',
-        success:'#42c486',
-        light:'#ffeae6'
+        primary: "#ebf9fc",
+        secondary: "#fe5634",
+        main: "#c6c8cc",
+        success: "#42c486",
+        light: "#ffeae6",
+        layout: "#FFAA33",
       },
       fontFamily: {
-        vazir: ['Vazir'],
+        vazir: ["Vazir"],
       },
     },
-    
-    
   },
   variants: {
     extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
