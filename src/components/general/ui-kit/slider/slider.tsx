1067da8a8dcdeeb5c7777721e136a56fc313135a
Sara Zamani <szamani240@gmail.com>
2024-03-23 12:48:30 +0330

    add dashboard design

--- src/components/general/ui-kit/slider/slider.tsx
+++ src/components/general/ui-kit/slider/slider.tsx
@@ -4,6 +4,7 @@ import Card from "@components/card/card";
 
 import { FaAngleLeft } from "react-icons/fa6";
 import { FaAngleRight } from "react-icons/fa6";
+
 import Title from "../title/title";
 
 interface CardType {
--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -1,13 +1,62 @@
-function Dashboard() {
+import { FC, useState } from "react";
+
+import { FaAngleLeft } from "react-icons/fa6";
+import { FaAngleRight } from "react-icons/fa6";
+import { FaBowlFood } from "react-icons/fa6";
+import { FaCalendarDays } from "react-icons/fa6";
+import { FaChartSimple } from "react-icons/fa6";
+import { FaAddressBook } from "react-icons/fa6";
+
+const Dashboard: FC = () => {
+  const [open, setOpen] = useState(true);
+  const Menus = [
+    { title: "داشبورد", icon: <FaChartSimple />, gap: false },
+    { title: "حساب کاربری", icon: <FaAddressBook />, gap: false },
+    { title: "لیست غذا", icon: <FaBowlFood />, gap: true },
+    { title: "افرودن غذا", icon: <FaCalendarDays />, gap: false },
+  ];
   return (
     <div className="flex">
-      <div className={`w-72 h-screen bg-layout`}>Sidebar</div>
+      <div
+        className={`${
+          open ? `w-64` : `w-20`
+        } duration-300 relative h-screen bg-layout`}
+      >
+        <div
+          className="absolute w-7 h-7 border-2  cursor-pointer -right-3 top-9  border-layout rounded-full text-center bg-white flex items-center justify-center"
+          onClick={() => setOpen((prevState) => !prevState)}
+        >
+          {open ? <FaAngleLeft /> : <FaAngleRight />}
+        </div>
+        <ul className="font-vazir pt-6">
+          {Menus.map((menu, index) => (
+            <li
+              className={`${
+                open ? "w-10/12" : "w-2 justify-center"
+              } w-10/12 text-white cursor-pointer text-md flex items-center mx-auto gap-x-4 hover:bg-crystal rounded-md px-5 py-3 ${
+                menu.gap ? "mt-9" : " mt-2"
+              }`}
+              key={index}
+            >
+              <div>{menu.icon}</div>
+              <span
+                className={`text-sm ${
+                  !open && "hidden"
+                } origin-left duration-200`}
+              >
+                {menu.title}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+
       <div className="p-7 text-2xl font-semibold flex-1 h-screen">
         <h1>Home page</h1>
         <div></div>
       </div>
     </div>
   );
-}
+};
 
 export default Dashboard;
