d66c6f1c1da2e19db1fe3dde472f6ad44af2e442
Sara Zamani <szamani240@gmail.com>
2024-03-27 23:02:04 +0330

    add path hinter component

--- src/routes/dashboard/dashboard.tsx
+++ src/routes/dashboard/dashboard.tsx
@@ -3,6 +3,7 @@ import AddFoods from "@components/add-foods/add-foods";
 import FoodTable from "@components/food-table/food-table";
 import Sidebar from "@components/general/layouts/sidebar/sidebar";
 import { Sample } from "@components/sample";
+import PathHinter from "@components/general/layouts/path-hinter/path-hinter";
 
 const Dashboard: FC = () => {
   const [activeId, setActiveId] = useState(0);
@@ -27,6 +28,7 @@ const Dashboard: FC = () => {
     <div className="flex font-vazir">
       <Sidebar sendActiveId={reciveActiveId} />
       <div className="p-7 text-2xl font-semibold flex-1 h-screen bg-slate-100 flex flex-grow flex-col">
+        <PathHinter />
         {renderPages()}
       </div>
     </div>
