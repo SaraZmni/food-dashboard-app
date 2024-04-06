2401d12fe59f616130fe140b20c572e486790e27
Emma <Emma@fanap.ir>
2024-04-06 13:28:08 +0330

    change dashboard card offset component

--- src/components/general/layouts/path-hinter/path-hinter.tsx
+++ src/components/general/layouts/path-hinter/path-hinter.tsx
@@ -13,22 +13,10 @@ const PathHinter: FC = () => {
           <div className="flex items-center">
             <a
               className="text-sm font-medium text-slate-700 hover:text-slate-400 dark:text-slate-400 dark:hover:text-white"
-              href="/"
+              href="/dashboard"
             >
               داشبورد
             </a>
-            {/* <svg
-              className="ml-1 h-6 w-6 text-slate-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fill-rule="evenodd"
-                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                clip-rule="evenodd"
-              ></path>
-            </svg> */}
             <FaAngleLeft className="mx-2 h-4 w-4 text-slate-400" />
           </div>
         </li>
