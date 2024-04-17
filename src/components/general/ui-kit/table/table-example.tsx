c603c596f794548d32c63f212f22212801c07416
Emma <Emma@fanap.ir>
2024-04-17 17:56:42 +0330

    add new table component

---
+++ src/components/general/ui-kit/table/table-example.tsx
@@ -0,0 +1,79 @@
+import { FC } from "react";
+// import { FaPencil, FaRegTrashCan } from "react-icons/fa6";
+
+const Table: FC = () => {
+  return (
+    // <div className="mt-8 flex flex-col" dir="rtl">
+    //   <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+    //     <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+    //       <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+    //         <table className="min-w-full divide-y divide-slate-300">
+    //           <thead className="bg-slate-50">
+    //             <tr>
+    //               <th
+    //                 scope="col"
+    //                 className="py-3.5 pl-4 pr-3 text-right text-sm font-semibold text-slate-900 sm:pl-6"
+    //               >
+    //                 نام غذا
+    //               </th>
+    //               <th
+    //                 scope="col"
+    //                 className="py-3.5 pl-4 pr-3 text-right text-sm font-semibold text-slate-900 sm:pl-6"
+    //               >
+    //                 تاریخ سفارش
+    //               </th>
+    //               <th
+    //                 scope="col"
+    //                 className="py-3.5 pl-4 pr-3 text-right text-sm font-semibold text-slate-900 sm:pl-6"
+    //               >
+    //                 وعده غذایی
+    //               </th>
+    //               <th
+    //                 scope="col"
+    //                 className="px-3 py-3.5 text-right text-sm font-semibold text-slate-900"
+    //               >
+    //                 <span className="sr-only">Edit</span>
+    //               </th>
+    //               <th scope="col" className="relative py-3.5 pr-4 sm:pr-6">
+    //                 <span className="sr-only">Delete</span>
+    //               </th>
+    //             </tr>
+    //           </thead>
+    //           <tbody className="divide-y divide-slate-200 bg-white">
+    //             <tr>
+    //               <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+    //                 <div className="font-medium text-slate-900">قرمه سبزی</div>
+    //               </td>
+    //               <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+    //                 <div className="flex flex-col items-start">
+    //                   <div className="font-medium text-slate-900">سه شنبه</div>
+    //                   <div className="text-slate-500">سیزده فروردین 20:30</div>
+    //                 </div>
+    //               </td>
+    //               <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
+    //                 <div className="flex flex-col items-start">
+    //                   <div className="font-medium text-slate-900">ناهار</div>
+    //                 </div>
+    //               </td>
+    //               <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+    //                 <div className="text-blue-600 hover:text-blue-900 cursor-pointer">
+    //                   <FaPencil />
+    //                 </div>
+    //               </td>
+    //               <td className="relative whitespace-nowrap py-4 pr-4 text-right text-sm font-medium sm:pr-6">
+    //                 <div className="text-rose-600 hover:text-rose-900 cursor-pointer">
+    //                   <FaRegTrashCan />
+    //                 </div>
+    //               </td>
+    //             </tr>
+    //           </tbody>
+    //         </table>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+    <div className="p-2 max-w-5xl mx-auto fill-gray-400"></div>
+  );
+};
+
+export default Table;
