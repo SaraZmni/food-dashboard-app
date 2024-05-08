18e79c667c3d325f1b9454224942120760ffdea1
Emma <Emma@fanap.ir>
2024-05-08 14:01:44 +0330

    customize order page

--- src/components/order-panel/order.panel.tsx
+++ src/components/order-panel/order.panel.tsx
@@ -14,25 +14,25 @@ const OrderPanel: FC = () => {
         return <div key={item.id}>{item.title}</div>;
       })} */}
 
-      <div className="container mx-auto mt-10">
+      <div className="container mx-auto mt-10 font-vazir" dir="rtl">
         <div className="flex shadow-md my-10">
           <div className="w-3/4 bg-white px-10 py-10">
             <div className="flex justify-between border-b pb-8">
-              <h1 className="font-semibold text-2xl">Shopping Cart</h1>
-              <h2 className="font-semibold text-2xl">3 Items</h2>
+              <h1 className="font-semibold text-2xl">لیست سفارشات</h1>
+              <h2 className="font-semibold text-2xl">3 آیتم</h2>
             </div>
             <div className="flex mt-10 mb-5">
               <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">
-                Product Details
+                جزییات سفارش
               </h3>
               <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">
-                Quantity
+                تعداد
               </h3>
               <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
-                Price
+                قیمت
               </h3>
               <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5 ">
-                Total
+                جمع کل
               </h3>
             </div>
             <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
@@ -51,7 +51,7 @@ const OrderPanel: FC = () => {
                     href="#"
                     className="font-semibold hover:text-red-500 text-gray-500 text-xs"
                   >
-                    Remove
+                    حذف
                   </a>
                 </div>
               </div>
@@ -100,7 +100,7 @@ const OrderPanel: FC = () => {
                     href="#"
                     className="font-semibold hover:text-red-500 text-gray-500 text-xs"
                   >
-                    Remove
+                    حذف
                   </a>
                 </div>
               </div>
@@ -149,7 +149,7 @@ const OrderPanel: FC = () => {
                     href="#"
                     className="font-semibold hover:text-red-500 text-gray-500 text-xs"
                   >
-                    Remove
+                    حذف
                   </a>
                 </div>
               </div>
@@ -185,27 +185,27 @@ const OrderPanel: FC = () => {
               href="#"
               className="flex font-semibold text-indigo-600 text-sm mt-10"
             >
+              بازگشت به صفحه سفارشات
               <svg
                 className="fill-current mr-2 text-indigo-600 w-4"
                 viewBox="0 0 448 512"
               >
                 <path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" />
               </svg>
-              Continue Shopping
             </a>
           </div>
 
           <div id="summary" className="w-1/4 px-8 py-10">
             <h1 className="font-semibold text-2xl border-b pb-8">
-              Order Summary
+              خلاصه سفارشات
             </h1>
             <div className="flex justify-between mt-10 mb-5">
-              <span className="font-semibold text-sm uppercase">Items 3</span>
+              <span className="font-semibold text-sm uppercase"> 3 آیتم</span>
               <span className="font-semibold text-sm">590$</span>
             </div>
             <div>
               <label className="font-medium inline-block mb-3 text-sm uppercase">
-                Shipping
+                استاندارد خرید
               </label>
               <select className="block p-2 text-gray-600 w-full text-sm">
                 <option>Standard shipping - $10.00</option>
@@ -216,25 +216,25 @@ const OrderPanel: FC = () => {
                 htmlFor="promo"
                 className="font-semibold inline-block mb-3 text-sm uppercase"
               >
-                Promo Code
+                کد تخفیف
               </label>
               <input
                 type="text"
                 id="promo"
-                placeholder="Enter your code"
+                placeholder="لطفا کد خود را وارد کنید"
                 className="p-2 text-sm w-full"
               />
             </div>
             <button className="bg-red-500 hover:bg-red-600 px-5 py-2 text-sm text-white uppercase">
-              Apply
+              تغییر
             </button>
             <div className="border-t mt-8">
               <div className="flex font-semibold justify-between py-6 text-sm uppercase">
-                <span>Total cost</span>
+                <span>مجموع هزینه</span>
                 <span>$600</span>
               </div>
               <button className="bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase w-full">
-                Checkout
+                تسویه حساب
               </button>
             </div>
           </div>
