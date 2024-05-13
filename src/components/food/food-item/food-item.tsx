0e3dc2b44aa9bf03f0c9029be2262ba640ffb427
Emma <Emma@fanap.ir>
2024-05-13 17:46:31 +0330

    add item with zustand

--- src/components/food/food-item/food-item.tsx
+++ src/components/food/food-item/food-item.tsx
@@ -14,6 +14,7 @@ const FoodItem: FC<FoodItemProps> = ({
   price,
   addOrderBoxItem,
 }) => {
+  console.log("addOrderBoxItem", addOrderBoxItem);
   return (
     <div className="bg-white border border-gray-100 transition transform duration-700 hover:shadow-xl hover:scale-105 p-4 rounded-lg relative w-1/4">
       <span className="bg-red-100 border border-secondary rounded-full text-secondary text-sm poppins px-4 py-1 inline-block mb-4 ">
@@ -33,7 +34,7 @@ const FoodItem: FC<FoodItemProps> = ({
         <h2 className="text-gray-900 poppins text-2xl font-bold">{price}</h2>
         <button
           className="bg-secondary text-white text-sm px-8 py-2 focus:outline-none poppins rounded-full mt-24 transform transition duration-300 hover:scale-105"
-          //   onClick={handleRoute}
+          onClick={() => addOrderBoxItem()}
         >
           افزودن به سبد خرید
         </button>
--- src/store/order-box/index.ts
+++ src/store/order-box/index.ts
@@ -2,13 +2,36 @@ import { create } from "zustand";
 import { OrderBox } from "./types";
 
 
-export const useOrderBox = create<OrderBox>(() => ({
+export const useOrderBox = create<OrderBox>((set,get) => ({
     items:[],
     invoice:{
         totalPrice:0
     },
     actions: {
-        addOrderBoxItem: (item) => {console.log('item', item)},
+        addOrderBoxItem: (item) => {
+            const foodAlreadyExist = get().items.some(_item => _item.id === item.id)
+            if(foodAlreadyExist){
+                return set((oldOrderBox) => ({
+                    invoice:{
+                        totalPrice:oldOrderBox.invoice.totalPrice + item.price
+                    },
+                    items:oldOrderBox.items.map(_item => {
+                        if(_item.id === item.id){
+                            return {
+                                ..._item,
+                                quantity:_item.quantity + 1 ,
+                            }
+                        }
+                        return _item
+                    })
+                }))
+            }
+
+            set((oldOrderBox) => ({
+                invoice:{totalPrice:oldOrderBox.invoice.totalPrice + item.price},
+                items:[...oldOrderBox.items,{...item,quantity:1}]
+            }))
+        },
         removeOrderBoxItem:(item) => {console.log('item', item)}
     }
 
